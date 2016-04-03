49315f64639dbb2be6a9a7cba368a873c95ef056
airburst <mark@fairhursts.net>
2016-04-03 14:00:53 +0100

    Suppressed hover and click logging from chart.ts.

--- app/chart.component.js
+++ app/chart.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/common', 'ng2-charts/ng2-charts'], function(exports_1, context_1) {
+System.register(['angular2/core', 'ng2-charts'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,31 +10,26 @@ System.register(['angular2/core', 'angular2/common', 'ng2-charts/ng2-charts'], f
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, common_1, ng2_charts_1;
+    var core_1, ng2_charts_1;
     var ElevationChart;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (common_1_1) {
-                common_1 = common_1_1;
-            },
             function (ng2_charts_1_1) {
                 ng2_charts_1 = ng2_charts_1_1;
             }],
         execute: function() {
             ElevationChart = (function () {
-                //@Input() elevation: number[];
                 function ElevationChart() {
                     this.lineChartData = [
-                        [65, 59, 80, 81, 56, 55, 40],
-                        [28, 48, 40, 19, 86, 27, 90],
-                        [18, 48, 77, 9, 100, 27, 40]
+                        [65, 59, 80, 81, 56, 55, 40]
                     ];
                     this.lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-                    this.lineChartSeries = ['Series A', 'Series B', 'Series C'];
+                    this.lineChartSeries = ['Test'];
                     this.lineChartOptions = {
+                        maintainAspectRatio: false,
                         animation: false,
                         responsive: true,
                         multiTooltipTemplate: '<%if (datasetLabel){%><%=datasetLabel %>: <%}%><%= value %>'
@@ -43,45 +38,23 @@ System.register(['angular2/core', 'angular2/common', 'ng2-charts/ng2-charts'], f
                         {
                             fillColor: 'rgba(148,159,177,0.2)',
                             strokeColor: 'rgba(148,159,177,1)',
-                            pointColor: 'rgba(148,159,177,1)',
-                            pointStrokeColor: '#fff',
-                            pointHighlightFill: '#fff',
-                            pointHighlightStroke: 'rgba(148,159,177,0.8)'
-                        },
-                        {
-                            fillColor: 'rgba(77,83,96,0.2)',
-                            strokeColor: 'rgba(77,83,96,1)',
-                            pointColor: 'rgba(77,83,96,1)',
-                            pointStrokeColor: '#fff',
-                            pointHighlightFill: '#fff',
-                            pointHighlightStroke: 'rgba(77,83,96,1)'
-                        },
-                        {
-                            fillColor: 'rgba(148,159,177,0.2)',
-                            strokeColor: 'rgba(148,159,177,1)',
-                            pointColor: 'rgba(148,159,177,1)',
-                            pointStrokeColor: '#fff',
-                            pointHighlightFill: '#fff',
-                            pointHighlightStroke: 'rgba(148,159,177,0.8)'
                         }
                     ];
-                    this.lineChartLegend = true;
+                    this.lineChartLegend = false;
                     this.lineChartType = 'Line';
-                    console.log('bar demo');
                 }
-                // events
                 ElevationChart.prototype.chartClicked = function (ev) {
-                    console.log(ev);
+                    console.log('click: ', ev);
                 };
                 ElevationChart.prototype.chartHovered = function (ev) {
-                    console.log(ev);
+                    //console.log('hover: ', ev);
                 };
                 ElevationChart = __decorate([
                     core_1.Component({
                         selector: 'elevation-chart',
-                        template: "\n        <base-chart class=\"chart\"\n                [data]=\"lineChartData\"\n                [labels]=\"lineChartLabels\"\n                [options]=\"lineChartOptions\"\n                [series]=\"lineChartSeries\"\n                [colours]=\"lineChartColours\"\n                [legend]=\"lineChartLegend\"\n                [chartType]=\"lineChartType\"\n                (chartHover)=\"chartHovered($event)\"\n                (chartClick)=\"chartClicked($event)\"></base-chart>\n    ",
+                        template: "\n        <base-chart class=\"chart\"\n            [data]=\"lineChartData\"\n            [labels]=\"lineChartLabels\"\n            [options]=\"lineChartOptions\"\n            [series]=\"lineChartSeries\"\n            [colours]=\"lineChartColours\"\n            [legend]=\"lineChartLegend\"\n            [chartType]=\"lineChartType\"\n            (chartHover)=\"chartHovered($event)\"\n            (chartClick)=\"chartClicked($event)\"\n            ></base-chart>\n    ",
                         styles: ["\n        .chart {\n            display: flex;\n            height: 244px;\n        }\n    "],
-                        directives: [ng2_charts_1.CHART_DIRECTIVES, common_1.NgClass, common_1.CORE_DIRECTIVES, common_1.FORM_DIRECTIVES]
+                        directives: [ng2_charts_1.CHART_DIRECTIVES]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], ElevationChart);
--- app/chart.component.ts
+++ app/chart.component.ts
@@ -1,20 +1,20 @@
 import {Component, EventEmitter, Input} from 'angular2/core';
-import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass} from 'angular2/common';
-import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
+import {CHART_DIRECTIVES} from 'ng2-charts';
 
 @Component({
     selector: 'elevation-chart',
     template: `
         <base-chart class="chart"
-                [data]="lineChartData"
-                [labels]="lineChartLabels"
-                [options]="lineChartOptions"
-                [series]="lineChartSeries"
-                [colours]="lineChartColours"
-                [legend]="lineChartLegend"
-                [chartType]="lineChartType"
-                (chartHover)="chartHovered($event)"
-                (chartClick)="chartClicked($event)"></base-chart>
+            [data]="lineChartData"
+            [labels]="lineChartLabels"
+            [options]="lineChartOptions"
+            [series]="lineChartSeries"
+            [colours]="lineChartColours"
+            [legend]="lineChartLegend"
+            [chartType]="lineChartType"
+            (chartHover)="chartHovered($event)"
+            (chartClick)="chartClicked($event)"
+            ></base-chart>
     `,
     styles: [`
         .chart {
@@ -22,24 +22,20 @@ import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
             height: 244px;
         }
     `],
-    directives: [CHART_DIRECTIVES, NgClass, CORE_DIRECTIVES, FORM_DIRECTIVES]
+    directives: [CHART_DIRECTIVES]
 })
 
 export class ElevationChart {
-    //@Input() elevation: number[];
 
-    constructor() {
-        console.log('bar demo');
-    }
+    constructor() { }
 
     private lineChartData: Array<any> = [
-        [65, 59, 80, 81, 56, 55, 40],
-        [28, 48, 40, 19, 86, 27, 90],
-        [18, 48, 77, 9, 100, 27, 40]
+        [65, 59, 80, 81, 56, 55, 40]
     ];
     private lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    private lineChartSeries: Array<any> = ['Series A', 'Series B', 'Series C'];
+    private lineChartSeries: Array<any> = ['Test'];
     private lineChartOptions: any = {
+        maintainAspectRatio: false,
         animation: false,
         responsive: true,
         multiTooltipTemplate: '<%if (datasetLabel){%><%=datasetLabel %>: <%}%><%= value %>'
@@ -48,38 +44,21 @@ export class ElevationChart {
         { // grey
             fillColor: 'rgba(148,159,177,0.2)',
             strokeColor: 'rgba(148,159,177,1)',
-            pointColor: 'rgba(148,159,177,1)',
-            pointStrokeColor: '#fff',
-            pointHighlightFill: '#fff',
-            pointHighlightStroke: 'rgba(148,159,177,0.8)'
-        },
-        { // dark grey
-            fillColor: 'rgba(77,83,96,0.2)',
-            strokeColor: 'rgba(77,83,96,1)',
-            pointColor: 'rgba(77,83,96,1)',
-            pointStrokeColor: '#fff',
-            pointHighlightFill: '#fff',
-            pointHighlightStroke: 'rgba(77,83,96,1)'
-        },
-        { // grey
-            fillColor: 'rgba(148,159,177,0.2)',
-            strokeColor: 'rgba(148,159,177,1)',
-            pointColor: 'rgba(148,159,177,1)',
-            pointStrokeColor: '#fff',
-            pointHighlightFill: '#fff',
-            pointHighlightStroke: 'rgba(148,159,177,0.8)'
+            // pointColor: 'rgba(148,159,177,1)',
+            // pointStrokeColor: '#fff',
+            // pointHighlightFill: '#fff',
+            // pointHighlightStroke: 'rgba(148,159,177,0.8)'
         }
     ];
-    private lineChartLegend: boolean = true;
+    private lineChartLegend: boolean = false;
     private lineChartType: string = 'Line';
 
-    // events
     chartClicked(ev: any) {
-        console.log(ev);
+        console.log('click: ', ev);
     }
 
     chartHovered(ev: any) {
-        console.log(ev);
+        //console.log('hover: ', ev);
     }
 
 }
