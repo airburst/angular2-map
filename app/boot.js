5d668395a58be9e21505965308027de60596a9ae
airburst <mark@fairhursts.net>
2016-05-05 11:27:42 +0100

    Include ToastOptions in boot.ts to set position of toasts

--- app/boot.js
+++ app/boot.js
@@ -1,11 +1,15 @@
-System.register(['angular2/platform/browser', 'angular2/http', 'angular2/router', './root', '@ngrx/store', './reducers/track', './reducers/markers', './reducers/elevation', './reducers/details', './reducers/gazetteer'], function(exports_1, context_1) {
+System.register(['angular2/platform/browser', 'angular2/core', 'angular2/http', 'angular2/router', './root', 'ng2-toastr/ng2-toastr', '@ngrx/store', './reducers/track', './reducers/markers', './reducers/elevation', './reducers/details', './reducers/gazetteer'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var browser_1, http_1, router_1, root_1, store_1, track_1, markers_1, elevation_1, details_1, gazetteer_1;
+    var browser_1, core_1, http_1, router_1, root_1, ng2_toastr_1, store_1, track_1, markers_1, elevation_1, details_1, gazetteer_1;
+    var options;
     return {
         setters:[
             function (browser_1_1) {
                 browser_1 = browser_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
             },
             function (http_1_1) {
                 http_1 = http_1_1;
@@ -15,6 +19,9 @@ System.register(['angular2/platform/browser', 'angular2/http', 'angular2/router'
             },
             function (root_1_1) {
                 root_1 = root_1_1;
+            },
+            function (ng2_toastr_1_1) {
+                ng2_toastr_1 = ng2_toastr_1_1;
             },
             function (store_1_1) {
                 store_1 = store_1_1;
@@ -35,10 +42,14 @@ System.register(['angular2/platform/browser', 'angular2/http', 'angular2/router'
                 gazetteer_1 = gazetteer_1_1;
             }],
         execute: function() {
+            options = {
+                positionClass: 'toast-bottom-right',
+            };
             browser_1.bootstrap(root_1.RootComponent, [
                 store_1.provideStore({ track: track_1.track, elevation: elevation_1.elevation, details: details_1.details, markers: markers_1.markers, results: gazetteer_1.results }),
                 http_1.HTTP_PROVIDERS,
-                router_1.ROUTER_PROVIDERS
+                router_1.ROUTER_PROVIDERS,
+                core_1.provide(ng2_toastr_1.ToastOptions, { useValue: new ng2_toastr_1.ToastOptions(options) })
             ]);
         }
     }
--- app/boot.ts
+++ app/boot.ts
@@ -1,8 +1,9 @@
 import {bootstrap} from 'angular2/platform/browser';
+import {provide} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {RootComponent} from './root';
-//import {AppComponent} from './app';
+import {ToastOptions} from 'ng2-toastr/ng2-toastr';
 import {provideStore} from '@ngrx/store';
 import {track} from './reducers/track';
 import {markers} from './reducers/markers';
@@ -10,8 +11,13 @@ import {elevation} from './reducers/elevation';
 import {details} from './reducers/details';
 import {results} from './reducers/gazetteer';
 
+let options = {
+    positionClass: 'toast-bottom-right',
+};
+    
 bootstrap(RootComponent, [
     provideStore({ track, elevation, details, markers, results }), 
     HTTP_PROVIDERS,
-    ROUTER_PROVIDERS
+    ROUTER_PROVIDERS,
+    provide(ToastOptions, { useValue: new ToastOptions(options)})
 ]);
